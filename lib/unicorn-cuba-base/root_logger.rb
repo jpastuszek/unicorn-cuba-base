5ec41e7e4a266377ec2243d5fb52baef2b74fd62
Jakub Pastuszek <jpastuszek@gmail.com>
2013-05-14 18:33:36 +0200

    refactored logging support

--- lib/unicorn-cuba-base/root_logger.rb
+++ lib/unicorn-cuba-base/root_logger.rb
@@ -4,16 +4,16 @@ class RootLogger < Logger
 	class ClassLogger
 		@@levels = [:debug, :info, :warn, :error, :fatal, :unknown]
 
-		def initialize(logger, class_obj)
-			@logger = logger
+		def initialize(root_logger, class_obj)
+			@root_logger = root_logger
 			@progname = class_obj.name
-			@logger.formatter = proc do |severity, datetime, progname, msg|
+			@root_logger.formatter = proc do |severity, datetime, progname, msg|
 				"[#{datetime.utc.strftime "%Y-%m-%d %H:%M:%S.%6N %Z"}] [#{$$} #{progname}] #{severity}: #{msg}\n"
 			end
 		end
 
 		def respond_to?(method)
-			@logger.respond_to? method
+			@root_logger.respond_to? method
 		end
 
 		def method_missing(name, *args, &block)
@@ -31,31 +31,63 @@ class RootLogger < Logger
 				end
 
 				# set program name to current class
-				@logger.progname = @progname
-				@logger.send(name, message, &block)
+				@root_logger.progname = @progname
+				@root_logger.send(name, message, &block)
 			else
-				@logger.send(name, *args, &block)
+				@root_logger.send(name, *args, &block)
 			end
+		end
+
+		attr_reader :root_logger
+
+		def inspect
+			"#<ClassLogger[#{@progname}] #{"0x%X" % object_id} root_logger=#{@root_logger.inspect}>"
 		end
 	end
 
 	def logger_for(class_obj)
 		ClassLogger.new(self, class_obj)
 	end
+
+	def inspect
+		"#<RootLogger #{"0x%X" % object_id}>"
+	end
 end
 
 module ClassLogging
-	def logger=(logger)
-		@@logger = logger
+	module ClassMethods
+		def init_logger
+			@@logger = {} unless defined? @@logger
+		end
+
+		def logger=(logger)
+			@@logger[self] = logger
+		end
+
+		def log
+			unless @@logger[self]
+				@@logger[self] = RootLogger::ClassLogger.new(Logger.new(STDERR), self)
+				@@logger[self].warn "new default logger crated"
+			end
+			@@logger[self]
+		end
+
+		def logger_for(class_obj)
+			RootLogger::ClassLogger.new(log.root_logger, class_obj)
+		end
 	end
 
 	def log
-		return @@logger if defined? @@logger
-		@@logger = Logger.new(STDERR)
+		self.class.log
+	end
+
+	def logger_for(class_obj)
+		self.class.logger_for(class_obj)
 	end
 
 	def self.included(class_obj)
-		class_obj.extend self
+		class_obj.extend ClassMethods
+		class_obj.init_logger
 	end
 end
 
