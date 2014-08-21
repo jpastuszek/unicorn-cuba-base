9177f9a71d8420426ce421f1f4110f3b75ed7a49
Jakub Pastuszek <jpastuszek@gmail.com>
2014-08-21 15:19:47 +0100

    decoding JavaScript encode() encoded strings

--- lib/unicorn-cuba-base/uri_ext.rb
+++ lib/unicorn-cuba-base/uri_ext.rb
@@ -7,8 +7,13 @@ module URI
 
 	# From http://en.wikipedia.org/wiki/Percent-encoding:
 	# The generic URI syntax mandates that new URI schemes that provide for the representation of character data in a URI must, in effect, represent characters from the unreserved set without translation, and should convert all other characters to bytes according to UTF-8, and then percent-encode those values.
+	# Also sometimes JavaScript encode() function (deprecated) is being used; this uses %uXXXX encoding for UTF-8 chars
 	def self.utf_decode(str)
-		pct_decode(str).force_encoding('UTF-8').tap do |u|
+		pct_decode(str) # decode %XX bits
+		.force_encoding('UTF-8') # Make sure the string is interpreting UTF-8 chars
+		.gsub(/%u([0-9a-z]{4})/) {|s| [$1.to_i(16)].pack("U")} # Decode %uXXXX encoded chars (JavaScript.encode())
+		.tap do |u|
+			# Validate fins string
 			raise URI::InvalidURIError, "invalid UTF-8 encoding in URI: #{u.inspect}" if not u.valid_encoding?
 		end
 	end
