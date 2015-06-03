947c39febee6c7f32fddd1493fb98f1b8b6aa151
Jakub Pastuszek <jpastuszek@gmail.com>
2015-06-03 12:18:34 +0100

    better performance stats with meta

--- lib/unicorn-cuba-base/perf_stats.rb
+++ lib/unicorn-cuba-base/perf_stats.rb
@@ -1,26 +1,31 @@
 require 'benchmark'
 
 $perf_stats_nest_level = -1
+$perf_stats_tasks = []
 
 module PerfStats
-	def measure(name)
+	def measure(task, object = nil)
 		$perf_stats_nest_level += 1
+		$perf_stats_tasks << task
+		task = $perf_stats_tasks.join(' -> ')
 		ret = nil
 		took = Benchmark.measure do
 			ret = yield
 		end
 		log.with_meta(
 			className: "PerfStats",
-			user: took.utime.round(6),
-			system:took.stime.round(6),
-			total: took.total.round(6),
-			real: took.real.round(6),
-			task: name,
+			user_time: took.utime.round(6),
+			system_time:took.stime.round(6),
+			total_time: took.total.round(6),
+			real_time: took.real.round(6),
+			task: task,
+			object: object,
 			level: $perf_stats_nest_level
-		).info "[PERF] #{"%0.6f" % took.real} sec -#{'+' * $perf_stats_nest_level} #{name} "
+		).info "took #{"%0.6f" % took.real} sec - #{task}#{object ? ": #{object}" : ''}"
 		ret
 	ensure
 		$perf_stats_nest_level -= 1
+		$perf_stats_tasks.pop
 	end
 end
 
